be0f367f621c7efc9d75076513511dfcb752ced7
generalimattia <mattigene@gmail.com>
2020-03-17 16:49:05 +0100

    Update versions

---
+++ reservation/src/main/java/com/generals/zimmerfrei/reservation/view/adapters/ColorsAdapter.kt
@@ -0,0 +1,42 @@
+package com.generals.zimmerfrei.reservation.view.adapters
+
+import android.graphics.Color
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.generals.zimmerfrei.reservation.R
+import de.hdodenhof.circleimageview.CircleImageView
+
+class ColorsAdapter(
+        private val colors: List<ReservationColor>
+): RecyclerView.Adapter<ColorViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_color, parent, false)
+        return ColorViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ColorViewHolder, position: Int) {
+        holder.bind(colors[position].color)
+    }
+
+    override fun getItemCount(): Int = colors.size
+
+    override fun getItemId(position: Int): Long = colors[position].id
+}
+
+class ColorViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    private val circleImageView: CircleImageView = view.findViewById(R.id.circle)
+
+    fun bind(color: String) {
+        circleImageView.circleBackgroundColor = Color.parseColor(color)
+    }
+}
+
+data class ReservationColor(
+        val id: Long,
+        val color: String,
+        var selected: Boolean
+)
